408231221e1c0eb025b82beeb5cbe2f637f43026
Ahmed Hussain <ahmedhussainnow@gmail.com>
2025-08-12 19:01:19 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [curPg, setCurPg] = useState(1);
+  const [postPerPg, setPostPerPg] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      console.log(response.data);
+      setPosts(response.data);
+      setLoading(false);
+    }
+    fetchPosts();
+  }, [])
+
+  return (
+    <>
+      <h1>Hi</h1>
+    </>
+  )
+}
+
+export default App
